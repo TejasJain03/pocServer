35ee75ec9e11c0b394d31f26c013c561e1f53fba
TejasJain03 <tejaskjain2003@gmail.com>
2026-05-11 14:38:49 +0530

    Booking API for creation

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/events', eventRoutes);
+
 app.use('/api/bookings', bookingRoutes);
 
 app.get('/', (req, res) => {
